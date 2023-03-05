789486d881af31d599b8d5536b79422f558fc90c
hectoradrian961030 <hectoradrian961030@gmail.com>
2023-03-06 00:53:19 +0100

    Add database sqlite3

--- chatbot/api.py
+++ chatbot/api.py
@@ -1,10 +1,42 @@
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.middleware.cors import CORSMiddleware
+from databases import Database
+
+database = Database("sqlite:///test.db")
 from fastapi import Request, FastAPI
+from google.cloud import dialogflow_v2 as dialogflow
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def database_connect():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def database_disconnect():
+    await database.disconnect()
+
 @app.post("/", tags=["Root"])
 async def read_root(request: Request):
-  print(await request.json())
-  return { 
-    "message": "Welcome to my notes application, use the /docs route to proceed"
-   }
+    data = request.json()
+    print(data)
+    session_client = dialogflow.SessionsClient()
+
+    session = data['session']
+    query_text = data['queryResult']['queryText']
+    action = data['queryResult']['action']
+
+    if action in ["interval_images.interval_images-no.interval_images_no-custom.interval_images_no_interval-custom-2", 
+                  "interval_images.interval_images-no.interval_images_no-custom.interval_images_no_interval-custom",
+                  "interval_images.interval_images-yes.interval_images_yes-custom-2",
+                  "interval_images.interval_images-yes.interval_images_yes-custom",
+                  "last_image.last_image-custom.last_image_location-custom-2",
+                  "last_image.last_image-custom.last_image_location-custom"]:
+        pass
+    else:
+        query = "INSERT INTO sessions (id, location, type) VALUES (:id, :location, :type)"
+        values = {"id": session.split('/')[-1], "location": query_text, "type": 'None'}
+        await database.execute(query=query, values=values)
+
+    
