7c2b07b0526ae27579b2bcf776f99b1284b02824
hectoradrian961030 <hectoradrian961030@gmail.com>
2023-03-06 01:45:57 +0100

    Add set interval or not

--- chatbot/api.py
+++ chatbot/api.py
@@ -26,13 +26,23 @@ async def read_root(request: Request):
     query_text = data['queryResult']['queryText']
     intent = data['queryResult']['intent']['displayName']
 
+    # OPTICA O RADAR
     if intent in ["interval_images_no_interval - custom-2", 
                   "interval_images_no_interval - custom",
                   "interval_images_yes - custom-2",
                   "interval_images_yes - custom",
                   "last_image_location - custom-2",
                   "last_image_location - custom"]:
-        pass
+        # OPTICA
+        if intent in ["interval_images_no_interval - custom",
+                  "interval_images_yes - custom",
+                  "last_image_location - custom"]:
+            pass
+        # RADAR
+        else:
+            pass
+
+            
     elif intent in ["interval_images"]:
         location = data['queryResult']['parameters']['location']['city']
         query = "INSERT INTO sessions (id, location, type, last) VALUES (:id, :location, :type, :last)"
@@ -42,5 +52,18 @@ async def read_root(request: Request):
         query = "INSERT INTO sessions (id, location, type, last) VALUES (:id, :location, :type, :last)"
         values = {"id": session, "location": "None", "type": 'None', "last": True}
         await database.execute(query=query, values=values)
+    elif intent in ["interval_images_yes",
+                    "interval_images_no"]:
+        # INTERVALO
+        if intent == "interval_images_yes":
+            last = False
+        # ULTIMA
+        else:
+            last = True
+
+        query = f"UPDATE sessions SET last = :new_last_value WHERE id = :row_id"
+        values = {"new_last_value": last, "row_id": session}
+        await database.execute(query=query, values=values)
+    
 
     
