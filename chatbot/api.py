d0531352de35ee150a9a55fa7d155f8a6d3836eb
hectoradrian961030 <hectoradrian961030@gmail.com>
2023-03-07 00:00:16 +0100

    Add get or create and prints

--- chatbot/api.py
+++ chatbot/api.py
@@ -16,15 +16,34 @@ async def database_connect():
 async def database_disconnect():
     await database.disconnect()
 
+async def get_or_create_chat(chat_id, location = 'None', type = 'None', last = False):
+    # Define the SQL query to get the session
+    query = f"SELECT * FROM sessions WHERE id = :session_id"
+
+    # Define the parameters for the query
+    values = {"session_id": chat_id}
+
+    # Execute the query and get the results
+    results = await database.fetch_all(query=query, values=values)
+
+    if not results:
+        query = "INSERT INTO sessions (id, location, type, last) VALUES (:id, :location, :type, :last) RETURNING ID"
+        values = {"id": chat_id, "location": location, "type": type, "last": last}
+        results = await database.execute(query=query, values=values)
+
 @app.post("/", tags=["Root"])
 async def read_root(request: Request):
     data = await request.json()
     print(data)
     print(request)
 
-    session = data['session'].split('/')[-1]
-    query_text = data['queryResult']['queryText']
+    chat_id = data['originalDetectIntentRequest']['payload']['data']['chat']['id']
     intent = data['queryResult']['intent']['displayName']
+
+    try:
+        location = data['queryResult']['parameters']['location']['city']
+    except:
+        location = 'None'
 
     # OPTICA O RADAR
     if intent in ["interval_images_no_interval - custom-2", 
@@ -33,36 +52,43 @@ async def read_root(request: Request):
                   "interval_images_yes - custom",
                   "last_image_location - custom-2",
                   "last_image_location - custom"]:
+        print("AAAAAAAA")
         # OPTICA
         if intent in ["interval_images_no_interval - custom",
                   "interval_images_yes - custom",
                   "last_image_location - custom"]:
-            pass
+            print("BBBBBBBB")
+            type = 'optica'
         # RADAR
         else:
-            pass
+            print("CCCCCCCC")
+            type = 'radar'
 
-            
+        query = f"UPDATE sessions SET type = :new_last_value WHERE id = :row_id"
+        values = {"new_type_value": last, "row_id": chat_id}
+        await database.execute(query=query, values=values)
+
+    # GET OR CREATE BY SESSION OR CHATID
     elif intent in ["interval_images"]:
-        location = data['queryResult']['parameters']['location']['city']
-        query = "INSERT INTO sessions (id, location, type, last) VALUES (:id, :location, :type, :last)"
-        values = {"id": session, "location": location, "type": 'None', "last": False}
-        await database.execute(query=query, values=values)
+        print("DDDDDDDD")
+        chat_id = get_or_create_chat(chat_id=chat_id, location=location, type='None', last=False)
     elif intent in ["last_image"]:
-        query = "INSERT INTO sessions (id, location, type, last) VALUES (:id, :location, :type, :last)"
-        values = {"id": session, "location": "None", "type": 'None', "last": True}
-        await database.execute(query=query, values=values)
+        print("EEEEEEEE")
+        chat_id = get_or_create_chat(chat_id=chat_id, location=location, type='None', last=True)
     elif intent in ["interval_images_yes",
                     "interval_images_no"]:
+        print("FFFFFFFF")
         # INTERVALO
         if intent == "interval_images_yes":
+            print("GGGGGGGG")
             last = False
         # ULTIMA
         else:
+            print("HHHHHHHH")
             last = True
 
         query = f"UPDATE sessions SET last = :new_last_value WHERE id = :row_id"
-        values = {"new_last_value": last, "row_id": session}
+        values = {"new_last_value": last, "row_id": chat_id}
         await database.execute(query=query, values=values)
     
 
